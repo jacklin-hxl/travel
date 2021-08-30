f285a1b3af17b4bedc8a7a950dae958c9c837017
Xuelin <19946174874@163.com>
2021-08-31 00:38:48 +0800

    update routeserviceimpl

--- src/main/java/cn/itcast/travel/service/impl/RouteServiceImpl.java
+++ src/main/java/cn/itcast/travel/service/impl/RouteServiceImpl.java
@@ -5,8 +5,13 @@ import cn.itcast.travel.domain.Route;
 import cn.itcast.travel.service.RouteService;
 
 public class RouteServiceImpl implements RouteService {
+    private
     @Override
     public PageBean<Route> pageQuery(int cid, int currentPage, int pageSize) {
+        //封装pageBean
+        PageBean<Route> pageBean = new PageBean<>();
+        pageBean.setCurrentPage(currentPage);
+        pageBean.setPageSize(pageSize);
 
     }
 }
